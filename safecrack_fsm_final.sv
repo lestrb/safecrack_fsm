da2e3941ab4ff26494faa0d665b332c1f889de10
Letícia Staudinger <lsr@cin.ufpe.br>
2025-08-14 21:07:32 -0300

    versão final

--- safecrack_fsm_final.sv
+++ safecrack_fsm_final.sv
@@ -24,27 +24,43 @@ module safecrack_fsm (
 
     state_t state, next;           // state pode assumir valores do typedef
     state_t state_before_error;    // guarda o estado antes do erro para voltar a ele
+	 state_t state_wait_release;    // guarda o estado antes do erro para voltar a ele
 
-    logic [3:0] passcode[2:0];     // armazena a senha (3 dígitos)
-    logic [1:0] qtd_erros;         // armazena a quantidade de erros (zerada no reset)
-    logic [1:0] qtd_acertos;       // armazena a quantidade de acertos (zerada no reset)
-    logic [3:0] segundos;          // contador de segundos no bloqueio (0–10)
-    logic [1:0] ms_index;          // índice para mudança de senha
-	 logic [25:0] div_count; 		  // 26 bits conseguem contar até 67 milhões
-	 logic        one_hz;           // pulso de 1 Hz
-		
+    logic [3:0] passcode[2:0]='{0: 4'b1110, 1: 4'b1101, 2: 4'b1011};
+    logic [1:0] qtd_erros = 2'b00;        // armazena a quantidade de erros (zerada no reset)
+    logic [1:0] qtd_acertos = 2'b00;      // armazena a quantidade de acertos (zerada no reset)
+    logic [3:0] segundos = 4'd0;          // contador de segundos no bloqueio (0–10)
+    logic [1:0] ms_index = 2'd0;          // índice para mudança de senha
+	 logic [25:0] div_count = 26'd0; 		// 26 bits conseguem contar até 67 milhões
+	 logic        one_hz = 1'b0;           // pulso de 1 Hz
+	 logic [3:0] last_btn_pressed;
+		 
 		
 	 // Reset assíncrono só no estado
 	 always_ff @(posedge clk or posedge rst) begin
 		 if (rst) begin
 			  state <= S0;
 			  state_before_error <= S0;  // estado inicial antes de erro
+			  state_wait_release <= S0;
 		 end else begin
 			  if (next == ERRO && state != ERRO) begin
 					state_before_error <= state;
 			  end
 		     if (state == S3) begin
 					state_before_error <= S0;  // estado inicial antes de erro
+			  end
+			  if (btn != 4'b1111 && next != state && next != ERRO) begin
+					// calcula próximo estado esperado com base no estado atual e botão pressionado
+					case (state)
+						  S0: state_wait_release <= (btn == passcode[0]) ? S1 : S0;
+						  S1: state_wait_release <= (btn == passcode[1]) ? S2 : S1;
+						  S2: state_wait_release <= (btn == passcode[2]) ? S3 : S2;
+						  MS0: state_wait_release <= MS0;
+						  MS1: state_wait_release <= MS1;
+						  MS2: state_wait_release <= MS2;
+						  default: state_wait_release <= state; 
+					endcase
+					last_btn_pressed <= btn; // guarda o botão pressionado
 			  end
 			  state <= next;
 		 end
@@ -87,8 +103,8 @@ module safecrack_fsm (
         // início do modo de mudança de senha
         if (ms) begin          
 			if (state == MS0 || state == MS1 || state == MS2) begin 
-				if (btn != 4'b1111) begin		  // captura apenas quando houver um botão válido (4'b1111 representa "nenhum botão pressionado")
-                    passcode[ms_index] <= btn;    // grava segundo botão
+				if (btn == 4'b1111 && state_wait_release != state) begin	  // só grava quando botão for solto e houver transição armazenada
+                    passcode[ms_index] <= last_btn_pressed;    // grava segundo botão
                     ms_index <= ms_index + 1;     // passa para próximo índice
                 end
             end else begin
@@ -100,8 +116,7 @@ module safecrack_fsm (
 		if (next == ERRO && state != ERRO) begin
             qtd_erros <= (qtd_erros < 3) ? qtd_erros + 1 : qtd_erros;
       end  
-		if ((state == S0 && next == S1) || (state == S1 && next == S2) || (state == S2 && next == S3)) begin
-			qtd_erros <= 2'b00; // Reseta erros ao acertar uma etapa
+		if ((state == S0 && btn == passcode[0]) || (state == S1 && btn == passcode[1]) || (state == S2 && btn == passcode[2])) begin
          qtd_acertos <= qtd_acertos + 1;	
 		end
 				
@@ -119,25 +134,55 @@ module safecrack_fsm (
     always_comb begin
         next = state; // valor padrão do próximo estado
         case (state) 
-            S0:	 begin  
-					   if (btn == 4'b1111)           next = S0; 	// se não tem botão pressionado, fica em S0
-					   else if (btn == passcode[0])  next = S1;     // se acertar o botão, vai pro próximo estado
-					   else						     next = ERRO;   // se não acertar botão, vai pra ERRO
-				   end
-            S1:    begin  
-					   if (btn == 4'b1111)           next = S1; 	// se não tem botão pressionado, fica em S1
-					   else if (btn == passcode[1])  next = S2;     // se acertar o botão, vai pro próximo estado
-					   else							 next = ERRO;   // se não acertar botão, vai pra ERRO
-				   end
-            S2:    begin  
-					   if (btn == 4'b1111)           next = S2; 	// se não tem botão pressionado, fica em S2
-					   else if (btn == passcode[2])  next = S3;     // se acertar o botão, vai pro próximo estado
-			     	   else							 next = ERRO;   // se não acertar botão, vai pra ERRO
-				   end
+            S0: begin // libera a transição armazenada quando o botão é solto
+						 if (btn == 4'b1111) begin
+							  next = (state_wait_release != S0) ? state_wait_release : S0; // libera transição armazenada
+						 end else begin
+							  next = (btn != passcode[0]) ? ERRO : S1;                     // botão errado → ERRO, botão certo → S1
+						 end
+					 end
+					
+            S1:  begin
+						 if (btn == 4'b1111) begin
+							  next = (state_wait_release != S1) ? state_wait_release : S1; // libera transição armazenada
+						 end else begin
+							  next = (btn != passcode[1]) ? ERRO : S2;                     // botão errado → ERRO, botão certo → S1
+						 end
+					 end
+
+            S2:  begin
+						 if (btn == 4'b1111) begin
+							  next = (state_wait_release != S2) ? state_wait_release : S2; // libera transição armazenada
+						 end else begin
+							  next = (btn != passcode[2]) ? ERRO : S3;                     // botão errado → ERRO, botão certo → S1
+						 end
+					 end   
+				
             S3:    next = (ms) ? MS0 : S3;                   // se ms ativo, vai pra estado de mudança de senha / se não, fica em S3 (unlocked)
-            MS0:   next = (btn != 4'b1111) ? MS1 : MS0;      // espera botão válido
-            MS1:   next = (btn != 4'b1111) ? MS2 : MS1;      // espera botão válido
-            MS2:   next = (btn != 4'b1111) ? S0  : MS2;      // espera botão válido
+            MS0:   begin
+							 if (btn == 4'b1111) begin
+								  if (state_wait_release != MS0)
+										next = state_wait_release; // libera transição armazenada
+								  else
+										next = MS0;
+							 end 
+						 end
+            MS1:   begin
+							 if (btn == 4'b1111) begin
+								  if (state_wait_release != MS1)
+										next = state_wait_release;
+								  else
+										next = MS1;
+							 end
+						 end
+            MS2:   begin
+							 if (btn == 4'b1111) begin
+								  if (state_wait_release != MS2)
+										next = state_wait_release;
+								  else
+										next = MS2;
+							 end
+						 end
             ERRO: begin
 						 if (btn == 4'b1111) begin
 							  if (qtd_erros >= 3)
@@ -159,9 +204,9 @@ module safecrack_fsm (
         unlocked = (state == S3); // destrava quando chega no estado S3
         
         // LEDs de erros (acende progressivamente conforme número de erros)
-        leds_erros[0] = (qtd_erros >= 2'd1) && !((state == S0 && next == S1) || (state == S1 && next == S2) || (state == S2 && next == S3));
-        leds_erros[1] = (qtd_erros >= 2'd2) && !((state == S0 && next == S1) || (state == S1 && next == S2) || (state == S2 && next == S3));
-        leds_erros[2] = (qtd_erros >= 2'd3) && !((state == S0 && next == S1) || (state == S1 && next == S2) || (state == S2 && next == S3));
+        leds_erros[0] = (qtd_erros >= 2'd1);
+        leds_erros[1] = (qtd_erros >= 2'd2);
+        leds_erros[2] = (qtd_erros >= 2'd3);
 
         // LEDs de acertos (acende progressivamente conforme número de acertos)
         leds_acertos[0] = (qtd_acertos >= 2'd1);
